e9c5be11e84dcd8ac117caf775642d656593a141
Daniel Bravo <dani.bravos@duocuc.cl>
2025-04-26 13:23:19 -0400

    Actualizacion de codigo

---
+++ src/main/java/com/example/Hospital_vm/repository/PacienteRepository.java
@@ -0,0 +1,13 @@
+package com.example.Hospital_vm.repository;
+
+import com.example.Hospital_vm.model.Paciente;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface PacienteRepository extends JpaRepository <Paciente,Integer> {
+
+}
